8e8a6203388d64bcdbeb3d4aaf0f6fa382909f1f
Jaehoon Jang <jangjh971008@naver.com>
2021-12-13 00:32:10 +0900

    jekyll theme 재설정

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello World!",
+        "excerpt":"안녕!   개발을 진행하며 생기는 이런저런 문제들과 해결방안들,   새롭게 적용해본 기술들, 습득한 지식들,   내가 꼭 기억해야겠다 싶은 내용들을 열심히 적어보자!  ","categories": ["ETC"],
+        "tags": [],
+        "url": "/etc/first-post/",
+        "teaser": null
+      },{
+        "title": "[Flutter] Runner.app/Frameworks/Flutter.framework does not support the minimum OS Version specified in the Info.plist",
+        "excerpt":"문제 상황 Flutter 를 이용한 첫 프로젝트 릴리즈를 위해 Xcode 에서 Archive 를 진행하던 도중, 다음과 같은 에러가 발생했다. [Flutter] Runner.app/Frameworks/Flutter.framework does not support the minimum OS Version specified in the Info.plist 처음에는 에러의 첫 부분을 제대로 읽지 않아서 단순하게 Info.plist 파일의 최소 OS 버전이 맞지 않는다고 생각했다. 동시에 Info.plist...","categories": ["Flutter"],
+        "tags": [],
+        "url": "/flutter/2/",
+        "teaser": null
+      }]
