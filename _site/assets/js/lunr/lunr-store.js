190aff0c88b6dd1e3b706241d6cc67e3f85774c3
Jaehoon Jang <jangjh971008@naver.com>
2021-12-30 22:04:13 +0900

    add: 2021-12-30-1.md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -14,6 +14,12 @@ var store = [{
         "title": "[Git] 커밋 취소하기, 되돌리기",
         "excerpt":"git reset Git 레포지토리를 이용하다 보면 commit 후 push 를 잘못 진행하거나, 브랜치 병합 후 push 과정에서 오류가 발생해 브랜치를 이전 commit 으로 되돌려야 하는 상황이 종종 발생합니다. 저같은 경우 이러한 상황이 자주 발생하지는 않지만 간혹 commit 을 되돌려야 할 때마다 커맨드가 헷갈리곤 해 확실히 정리하고자 합니다. 사용법 원하는 commit...","categories": ["Git"],
         "tags": [],
-        "url": "/git/3/",
+        "url": "/git/1/",
+        "teaser": null
+      },{
+        "title": "[Flutter] Stateless, Stateful Widget, LifeCycle",
+        "excerpt":"Stateless / Stateful Widget Flutter 의 Lifecycle, 생명주기를 이해하기 위해서는 Stateless Widget 과 Stateful Widget 의 차이점을 알아야 합니다. 여기서 State 란 위젯의 상태를 의미합니다. Stateless Widget Stateless Widget 은 변경 가능한 State 가 필요하지 않은 Widget 을 뜻합니다. 다시 말해 Stateless Widget 도 State 를 가지긴 하지만, 한 번...","categories": ["Flutter"],
+        "tags": [],
+        "url": "/flutter/1/",
         "teaser": null
       }]
