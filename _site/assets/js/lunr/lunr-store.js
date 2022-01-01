6a7cf05d196f4b275b8737a8b6babfa2134df667
Jaehoon Jang <jangjh971008@naver.com>
2022-01-02 00:17:41 +0900

    add 2022-01-02-3.md

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,7 +8,7 @@ var store = [{
         "title": "[Flutter] minimum OS Version 에러 발생",
         "excerpt":"ERROR? Runner.app/Frameworks/Flutter.framework does not support the minimum OS Version specified in the Info.plist 문제 상황 Flutter 를 이용한 첫 프로젝트 릴리즈를 위해 Xcode 에서 Archive 를 진행하던 도중, 다음과 같은 에러가 발생했다. [Flutter] Runner.app/Frameworks/Flutter.framework does not support the minimum OS Version specified in the Info.plist 처음에는 에러의 첫 부분을 제대로...","categories": ["Flutter"],
         "tags": [],
-        "url": "/flutter/2/",
+        "url": "/flutter/1/",
         "teaser": null
       },{
         "title": "[Git] 커밋 취소하기, 되돌리기",
@@ -20,6 +20,12 @@ var store = [{
         "title": "[Flutter] Stateless, Stateful Widget, LifeCycle",
         "excerpt":"Stateless / Stateful Widget Flutter 의 Lifecycle, 생명주기를 이해하기 위해서는 Stateless Widget 과 Stateful Widget 의 차이점을 알아야 합니다. 여기서 State 란 위젯의 상태를 의미합니다. Stateless Widget Stateless Widget 은 변경 가능한 State 가 필요하지 않은 Widget 을 뜻합니다. 다시 말해 Stateless Widget 도 State 를 가지긴 하지만, 한 번...","categories": ["Flutter"],
         "tags": [],
-        "url": "/flutter/1/",
+        "url": "/flutter/2/",
+        "teaser": null
+      },{
+        "title": "[Flutter] Null Safety",
+        "excerpt":"Flutter - null safety Flutter 2.0 에 들어서면서부터 null safety 가 적용되었다고 합니다. 오늘은 null safety 가 무엇이며 어떻게 쓰이는지 알아보도록 하겠습니다. null safety ? null safety 는 null 로 인해서 발생하는 에러들을 방지하기 위해 도입되었습니다. 실제로 코드를 작성하다가 보면 null 이 아니어야 하는 값에 null 이 삽입된다거나, 리턴된다거나 하는...","categories": ["Flutter"],
+        "tags": [],
+        "url": "/flutter/3/",
         "teaser": null
       }]
